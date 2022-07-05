e862bb6d65cb4f55203d0eefc9b78c7699130a89
roger <roger@DESKTOP-6BBSUVU>
2022-07-05 12:39:45 +0200

    Mi primer commit (TA02.1)

---
+++ BTTSRS_C1_TA02.1/src/ComentarioLinea.java
@@ -0,0 +1,14 @@
+/**
+ * @author Roger Rubio Rufaste
+ *
+ */
+public class ComentarioLinea {
+	/**
+	 * @param args
+	 */
+	public static void main(String args[]){
+		//System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");
+	}
+}
